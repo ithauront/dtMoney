5621c90b7beb9d8d0dce7f973fc074a0838fb6c0
IURI <123806396+ithauront@users.noreply.github.com>
2023-06-20 19:26:51 +0200

    made a context for the information fetched
    made a context to treat the information of transactions that will come from the back end.
    already imported this context in summary and in transactions

--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -1,31 +1,14 @@
-import { useEffect, useState } from "react";
+import { useContext } from "react";
 import { Header } from "../../components/header";
 import { Summary } from "../../components/summary";
 import { SearchForm } from "./components/searchForm";
 import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
+import { TransactionsContext } from "../../contexts/transactionsContext";
 
-interface Transaction {
-id: number,
-description: string,
-type: 'income' | 'outcome',
-price: number,
-category: string,
-createdAt: string
-}
+
 
 export function Transactions () {
-    const [transactions, setTransactions] = useState<Transaction[]>([])
-
-    async function loadTransactions() {
-        const response = await fetch('http://localhost:3000/transactions')
-        const data = await response.json();
-
-        setTransactions(data);
-    }
-useEffect(()=> {
-    loadTransactions()
-},[])
-
+   const {transactions} = useContext(TransactionsContext)
     return(
         <div>
             <Header />
